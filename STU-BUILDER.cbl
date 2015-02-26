e7915afe605728a050583e9c2c3822e4b0d73e62
Montana Ruth <mruth00@g.uafs.edu>
2015-02-26 09:46:11 -0600

    Added STU-BUILDER program template

---
+++ STU-BUILDER.cbl
@@ -0,0 +1,22 @@
+      ******************************************************************
+      *PROGRAM :  STU-BUILDER                                          *
+      *AUTHOR  : MONTANA RUTH                                          *
+      *DATE    :    Feb 26, 2015                                       *
+      *ABSTRACT:                                                       *
+      ******************************************************************
+        
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. STU-BUILDER
+       
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       
+       000-MAIN.
+       STOP RUN.
+       
+
