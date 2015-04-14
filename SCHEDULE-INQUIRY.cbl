6151ad4fa8a88e12709d5bc0f5eb225f3c7b6a84
cbaile02 <cbaile02@BD14701.student.ad.uafortsmith.edu>
2015-04-14 10:18:52 -0500

    ADDED SEARCH BY CRN SCHEDULE FUNCTIONALITY

--- SCHEDULE-INQUIRY.cbl
+++ SCHEDULE-INQUIRY.cbl
@@ -15,7 +15,12 @@
       *-----------------------------------------------------------------
        FD  IN-FILE.
        01  IN-REC.
-           03  SCHEDULE-ID-I       PIC X(12).
+           03  SCHEDULE-ID-I.
+               05  SCHEDULE-YEAR   PIC X(4).
+               05  FILLER          PIC X.
+               05  SCHEDULE-SEM    PIC XX.
+               05  FILLER          PIC X.
+               05  SCHEDULE-CRN    PIC X(4).
            03  FILLER              PIC X.
            03  COURSE-ID-I.
                05  COURSE-ID-DEPT  PIC X(4).
@@ -40,12 +45,14 @@
            03  WS-ANOTHER          PIC X VALUE 'Y'.
                88  ANOTHER                 VALUE 'N'.
            03  WS-COURSE-DEPT      PIC X(4).
+           03  WS-YEAR             PIC 9(4).
+           03  WS-SEM              PIC 99.
        01  WS-PG-BREAK.
            03  FILLER              PIC X(15) VALUE 'PRESS ENTER TO '.
            03  FILLER              PIC X(16) VALUE 'DISPLAY 10 MORE '.
            03  FILLER              PIC X(49) VALUE 'RECORDS'.
        01  WS-HEADER.
-           03  FILLER              PIC X(13) VALUE 'SCHEDULE ID'.
+           03  FILLER              PIC X(13) VALUE 'YEAR SEM CRN '. 
            03  FILLER              PIC X(10) VALUE 'COURSE ID'.
            03  FILLER              PIC X(21)  VALUE 'TIME        DAYS'.
            03  FILLER              PIC X(12) VALUE 'BLD/ROOM'.
@@ -58,20 +65,38 @@
        01  SCRN-TITLE.
            03  LINE 1  COL 30  VALUE 'SCHEDULE BY DEPARTMENT'.
        01  SCRN-DISPLAY.
+           03  LINE 3  COL 20  VALUE 'YEAR:'.
+           03          COL 30  PIC 9999 USING WS-YEAR
+                                       AUTO REQUIRED.
+           03  LINE 5  COL 20  VALUE 'SEMESTER'.
+           03          COL 30  PIC 99 USING WS-SEM
+                                       AUTO REQUIRED.
+           03  LINE 7  COL 20  VALUE 'DEPT'.
+           03          COL 30  PIC XXXX USING WS-COURSE-DEPT
+                                       AUTO REQUIRED.
        
        01  SCRN-ANOTHER.
            03  LINE 7  COL 32  VALUE 'LOOK UP ANOTHER? (Y/N)'.
            03          COL 30  PIC X TO WS-ANOTHER.
       *----------------------------------------------------------------- 
        PROCEDURE DIVISION.
-       000-MAIN.
-      *     PERFORM UNTIL ANOTHER
-      *         DISPLAY BLNK-SCRN
-           MOVE 'BIOL' TO WS-COURSE-DEPT.    
-           PERFORM 100-LIST.
-           DISPLAY 'PRESS ENTER TO GO BACK TO MENU'
+       000-MAIN. 
+           PERFORM UNTIL ANOTHER
+               MOVE ZEROS TO WS-YEAR,WS-SEM
+               MOVE SPACES TO WS-COURSE-DEPT
+               DISPLAY BLNK-SCRN
+               DISPLAY SCRN-DISPLAY
+               ACCEPT  SCRN-DISPLAY
+               PERFORM 100-LIST
+               DISPLAY 'PRESS ENTER TO GO BACK TO MENU'
+               ACCEPT WS-RESP
+               DISPLAY BLNK-SCRN
+               DISPLAY SCRN-ANOTHER
+               ACCEPT  SCRN-ANOTHER
+              
+           END-PERFORM.
+           DISPLAY 'PRESS ENTER TO GO BACK TO MENU'.
            ACCEPT WS-RESP.
-           CLOSE IN-FILE.
            EXIT PROGRAM.
            
            
@@ -87,7 +112,10 @@
                    AT END
                        MOVE 'Y' TO WS-EOF
                    NOT AT END
-                       IF COURSE-ID-DEPT EQUALS WS-COURSE-DEPT THEN
+                       IF SCHEDULE-YEAR EQUALS WS-YEAR
+                           AND SCHEDULE-SEM  EQUALS WS-SEM
+                           AND COURSE-ID-DEPT EQUALS WS-COURSE-DEPT
+                       THEN
                            DISPLAY IN-REC
                            DISPLAY WS-BLNK-LN
                            ADD 1 TO WS-COUNTER
